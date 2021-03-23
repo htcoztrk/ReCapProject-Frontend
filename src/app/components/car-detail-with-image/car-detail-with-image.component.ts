782d727f6a19fa73204b2304d8ac17970b42b6a3
htcoztrk <haticeozturk0207@gmail.com>
2021-03-23 18:57:51 +0300

    18.ders

--- src/app/components/car-detail-with-image/car-detail-with-image.component.ts
+++ src/app/components/car-detail-with-image/car-detail-with-image.component.ts
@@ -2,7 +2,9 @@ import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarDetail } from 'src/app/models/carDetail';
+import { CarImage } from 'src/app/models/carImage';
 import { CarDetailWithImageService } from 'src/app/services/car-detail-with-image.service';
+import { CarImageService } from 'src/app/services/car-image.service';
 
 @Component({
   selector: 'app-car-detail-with-image',
@@ -12,25 +14,44 @@ import { CarDetailWithImageService } from 'src/app/services/car-detail-with-imag
 export class CarDetailWithImageComponent implements OnInit {
 
   constructor(private carDetailWithImageService:CarDetailWithImageService,
+    private carImageService:CarImageService,
     private activatedRoute:ActivatedRoute) { }
-  carDetail:CarDetail[]=[];
+  carDetails:CarDetail[]=[];
+  carImages:CarImage[]=[];
   dataLoaded=false;
   message:string
   carii:number
-  path="C:/Users/Hp/Desktop/KodlamaİO/ReCapProject/WebAPI/Images/";
-  imageBaseUrl = "https://localhost:44368/";
+  currentImage:CarImage
   ngOnInit(): void {
         this.activatedRoute.params.subscribe(params=>{
-        this.getCarDetailByCarId(params["carId"])
+        this.getCarDetailByCarId(params["carId"]),
+        this.getCarImageByCarId(params["carId"])
       })
       
   }
   getCarDetailByCarId(carId:number){
      this.carDetailWithImageService.getCarDetailByCarId(carId).subscribe(response=>{
-       this.carDetail=response.data;
+       this.carDetails=response.data;
        this.message=response.message
        this.dataLoaded=true;
        this.carii=carId
      })
   }
+  getCarImageByCarId(carId:number){
+    this.carImageService.getImageByCarId(carId).subscribe(response=>{
+      this.carImages=response.data;
+      
+      this.dataLoaded=true;
+      this.currentImage=this.carImages[0];
+    })
+ }
+
+ getSliderClassName(carImage:CarImage){
+  if(this.currentImage ==carImage){
+    return "carousel-item active"
+  }else{
+    return "carousel-item"
+  }
+
+}
 }
--- src/app/services/car-image.service.ts
+++ src/app/services/car-image.service.ts
@@ -8,12 +8,17 @@ import { ListResponseModel } from '../models/listResponseModel';
   providedIn: 'root'
 })
 export class CarImageService {
-  apiUrl="https://localhost:44368/api/carImages/getall";
+  apiUrl="https://localhost:44368/api/";
 
   constructor(private httpClient:HttpClient) { }
 
   getCarImage():Observable<ListResponseModel<CarImage>>{
+    let newPath=this.apiUrl+"carImages/getall"
       return this.httpClient.
       get<ListResponseModel<CarImage>>(this.apiUrl);
+  }
+  getImageByCarId(carId:number):Observable<ListResponseModel<CarImage>>{
+    let newPath=this.apiUrl+"carImages/getcarimagesbycarid?carId="+carId
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath);
   }
 }
